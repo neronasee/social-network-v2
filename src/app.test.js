a4332086c095e8792ea049c77b1351ebd20de7b9
Oleksandr Vorona <ovorona@qualium-systems.com.ua>
2018-06-12 11:42:49 +0300

    added CI, fixed tests

--- src/app.test.js
+++ src/app.test.js
@@ -1,9 +1,17 @@
+const request = require('supertest');
 const app = require('./app');
-const request = require('supertest').agent(app.listen());
+
+let server;
+
+beforeEach(() => {
+  server = app.listen(5000);
+});
+
+afterEach(() => server.close());
 
 describe('test the root path', () => {
   test('it should respond with data', async () => {
-    const response = await request.get('/');
+    const response = await request(server).get('/healthcheck');
     expect(response.statusCode).toBe(200);
   });
 });
